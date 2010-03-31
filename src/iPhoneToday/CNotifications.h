8c4793c3b750b3722dec77254f8cbe2c910cc6ff
tronikos <tronikos@users.noreply.github.com>
2010-03-31 00:52:51 +0000

    Added Unplayed Voice Mails icon. Added Irda State icon.

--- src/iPhoneToday/CNotifications.h
+++ src/iPhoneToday/CNotifications.h
@@ -10,6 +10,7 @@
 
 enum dw_notifications_enum {
 	SN_PHONEMISSEDCALLS,				// HKCU\System\State\Phone\Missed Call Count
+	SN_MESSAGINGVOICEMAILTOTALUNREAD,	// HKCU\System\State\Messages\VMail\Total\Unread\Count
 	SN_MESSAGINGSMSUNREAD,				// HKCU\System\State\Messages\SMS\Unread\Count
 	SN_MESSAGINGMMSUNREAD,				// HKCU\System\State\Messages\MMS\Unread\Count
 	SN_MESSAGINGOTHEREMAILUNREAD,		// HKCU\System\State\Messages\OtherEmail\Unread\Count
@@ -25,6 +26,7 @@ enum dw_notifications_enum {
 	SN_CLOCKALARMFLAGS0,				// HKLM\Software\Microsoft\Clock\0\AlarmFlags
 	SN_CLOCKALARMFLAGS1,				// HKLM\Software\Microsoft\Clock\1\AlarmFlags
 	SN_CLOCKALARMFLAGS2,				// HKLM\Software\Microsoft\Clock\2\AlarmFlags
+	SN_IRDA,							// HKLM\Software\Microsoft\Obex\IsEnabled
 	SN_RELOADICON,						// HKLM\Software\iPhoneToday\reloadIcon
 	SN_RELOADICONS,						// HKLM\Software\iPhoneToday\reloadIcons
 	MAXDWORDNOTIFICATION
