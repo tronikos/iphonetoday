e3f4a99ba30b47f85c89858dea14eece96ce245d
tronikos <tronikos@65651da8-0ed0-11df-b706-cdd0a514c4f8>
2010-04-03 15:57:28 +0000

    Fixed a bug with bubbles

--- trunk/src/iPhoneToday/VersionNo.h
+++ trunk/src/iPhoneToday/VersionNo.h
@@ -3,4 +3,4 @@
 #define PRODUCTVER     1,5,1,1
 #define STRFILEVER     "1, 5, 1, 1\0"
 #define STRPRODUCTVER  "1, 5, 1, 1\0"
-#define LSTRPRODUCTVER L"v1.5.1 (Apr 2, 2010)"
+#define LSTRPRODUCTVER L"v1.5.1 (Apr 3, 2010)"
