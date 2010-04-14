ad3679d9da2162562d934b7a07da1c52832d4aae
tronikos <tronikos@65651da8-0ed0-11df-b706-cdd0a514c4f8>
2010-04-14 07:33:39 +0000

    Made the WriteToLog function accept arbitrary arguments (printf style).

--- trunk/src/iPhoneToday/Log.cpp
+++ trunk/src/iPhoneToday/Log.cpp
@@ -1,26 +1,17 @@
 #include "Log.h"
 
 /*
-HANDLE	g_hLogFile;
-TCHAR	g_szLogPath[MAX_PATH];
-
-
-// This function could write ASCII log files instead of unicode, but internationalization
-// was important for this sample
-void WriteToLog(LPTSTR tszLine)
+void WriteToLog(const TCHAR* message, ...)
 {
-	DWORD dwWritten;
+	FILE *f = _wfopen(L"\\iPhoneToday_log.txt", L"a+");
 
-	TCHAR*            ptszSystemDate = (TCHAR*)LocalAlloc(LPTR, MAX_PATH);
+	va_list args;
+	va_start(args, message);
+	vfwprintf(f, message, args);
+	//fwprintf(f, L"\r\n");
+	va_end(args);
+	fflush(f);
 
-	wsprintf(g_szLogPath, TEXT("\\log.txt"));
-	g_hLogFile = CreateFile(g_szLogPath, GENERIC_WRITE | GENERIC_READ, FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, 
-                OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL); 
-    SetFilePointer(g_hLogFile, 0, NULL, FILE_END);
-    
-	WriteFile(g_hLogFile, tszLine, _tcslen(tszLine) * sizeof(*tszLine), &dwWritten, NULL);
-            
-    CloseHandle(g_hLogFile);
-    LocalFree(ptszSystemDate);
+	fclose(f);
 }
 */
