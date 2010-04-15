47295e1fb98cb052e94e0b7aa391e88de21977aa
tronikos <tronikos@users.noreply.github.com>
2010-04-15 18:40:51 +0000

    Updated version information for release.

--- src/iPhoneToday/VersionNo.h
+++ src/iPhoneToday/VersionNo.h
@@ -3,4 +3,4 @@
 #define PRODUCTVER     1,5,2,1
 #define STRFILEVER     "1, 5, 2, 1\0"
 #define STRPRODUCTVER  "1, 5, 2, 1\0"
-#define LSTRPRODUCTVER L"v1.5.2 (Apr 14, 2010)"
+#define LSTRPRODUCTVER L"v1.5.2 (Apr 15, 2010)"
