ece734f0ccaac00325a4580d34c967787372eaf2
tronikos <tronikos@users.noreply.github.com>
2010-04-15 06:51:30 +0000

    Changed version to 1.5.2 for release

--- src/iPhoneToday/VersionNo.h
+++ src/iPhoneToday/VersionNo.h
@@ -1,6 +1,6 @@
-// 1.5.1
-#define FILEVER        1,5,1,1
-#define PRODUCTVER     1,5,1,1
-#define STRFILEVER     "1, 5, 1, 1\0"
-#define STRPRODUCTVER  "1, 5, 1, 1\0"
-#define LSTRPRODUCTVER L"v1.5.1 (Apr 3, 2010)"
+// 1.5.2
+#define FILEVER        1,5,2,1
+#define PRODUCTVER     1,5,2,1
+#define STRFILEVER     "1, 5, 2, 1\0"
+#define STRPRODUCTVER  "1, 5, 2, 1\0"
+#define LSTRPRODUCTVER L"v1.5.2 (Apr 14, 2010)"
