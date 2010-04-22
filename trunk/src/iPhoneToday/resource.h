5a191dc5b868d979b168c4a468a4442c0a95a215
tronikos <tronikos@65651da8-0ed0-11df-b706-cdd0a514c4f8>
2010-04-22 06:04:32 +0000

    Added "Delay 2nd part of animation for (ms)" option.

--- trunk/src/iPhoneToday/resource.h
+++ trunk/src/iPhoneToday/resource.h
@@ -114,6 +114,7 @@
 #define IDC_STATICURL                   1118
 #define IDC_CHECK_FREESTYLE_SCROLL      1124
 #define IDC_EDIT_ANIMATION_TIME         1125
+#define IDC_EDIT_ANIMATION_DELAY        1126
 #define IDC_EDIT_BACK_FACTOR            1142
 #define IDC_COMBO_CS                    1144
 #define IDC_CHECK_ONLAUNCH_SOUND        1145
