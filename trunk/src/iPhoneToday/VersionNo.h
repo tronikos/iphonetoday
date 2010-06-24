215cdbeed703503a5d3f99d76acf8458729d746b
tronikos <tronikos@65651da8-0ed0-11df-b706-cdd0a514c4f8>
2010-06-24 09:04:32 +0000

    Updated version to 1.5.3 for release.

--- trunk/src/iPhoneToday/VersionNo.h
+++ trunk/src/iPhoneToday/VersionNo.h
@@ -3,4 +3,4 @@
 #define PRODUCTVER     1,5,3,1
 #define STRFILEVER     "1, 5, 3, 1\0"
 #define STRPRODUCTVER  "1, 5, 3, 1\0"
-#define LSTRPRODUCTVER L"v1.5.3 beta (May 16, 2010)"
+#define LSTRPRODUCTVER L"v1.5.3 (June 24, 2010)"
