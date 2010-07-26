24c07b1f6c17bfd64c451850769f46c13eeb61b3
tronikos <tronikos@65651da8-0ed0-11df-b706-cdd0a514c4f8>
2010-07-26 11:17:42 +0000

    Updated version for release.

--- trunk/src/iPhoneToday/VersionNo.h
+++ trunk/src/iPhoneToday/VersionNo.h
@@ -1,6 +1,6 @@
-// 1.5.3
-#define FILEVER        1,5,3,1
-#define PRODUCTVER     1,5,3,1
-#define STRFILEVER     "1, 5, 3, 1\0"
-#define STRPRODUCTVER  "1, 5, 3, 1\0"
-#define LSTRPRODUCTVER L"v1.5.3 (June 24, 2010)"
+// 1.5.4
+#define FILEVER        1,5,4,1
+#define PRODUCTVER     1,5,4,1
+#define STRFILEVER     "1, 5, 4, 1\0"
+#define STRPRODUCTVER  "1, 5, 4, 1\0"
+#define LSTRPRODUCTVER L"v1.5.4 (July 26, 2010)"
